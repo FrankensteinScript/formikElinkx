464dea086499d09255309a3af1a731d435b0e3e4
Patrik <patrikludvik@gmail.com>
2026-01-30 11:29:22 +0100

    fix select placeholder

--- src/components/form/FormSelect.tsx
+++ src/components/form/FormSelect.tsx
@@ -1,4 +1,4 @@
-import { Select, MenuItem, FormControl, FormHelperText, InputLabel, type SelectProps } from "@mui/material";
+import { Select, MenuItem, FormControl, FormHelperText, InputLabel, type SelectProps, Typography } from "@mui/material";
 import { useField } from "formik";
 
 type Props = SelectProps & {
@@ -11,9 +11,26 @@ export const FormSelect = ({ name, label, menuItem, ...props }: Props) => {
 	const [field, meta, helpers] = useField(name);
 
 	return (
-		<FormControl fullWidth error={meta.touched && Boolean(meta.error)}>
+		<FormControl fullWidth sx={{ minWidth: 250 }} error={meta.touched && Boolean(meta.error)}>
 			<InputLabel shrink>{label}</InputLabel>
-			<Select {...field} {...props} value={field.value || ""} onChange={(e) => helpers.setValue(e.target.value)}>
+			<Select
+				{...field}
+				{...props}
+				value={field.value || ""}
+				onChange={(e) => helpers.setValue(e.target.value)}
+				displayEmpty={true}
+				renderValue={(value) =>
+					value?.length ? (
+						Array.isArray(value) ? (
+							value.join(", ")
+						) : (
+							value
+						)
+					) : (
+						<Typography color="textDisabled">Vyberte...</Typography>
+					)
+				}
+			>
 				{menuItem.map((item) => (
 					<MenuItem key={item} value={item}>
 						{item}
